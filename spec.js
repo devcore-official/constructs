bf162d29712869b2c9ccb07e4458e6ad08b7bb96
neur0plasticity <you@example.com>
2021-11-02 15:40:35 -0700

    construct funC and claS need testing

--- spec.js
+++ spec.js
@@ -1,5 +1,7 @@
 const alG = require("./alg.js");
-const funC = require("")
+const funC = require("./func.js");
+const claS = require("./clas.js");
+const apI = require("./apI.js");
 
 new alG("add3")
 	.description(`
@@ -53,7 +55,6 @@ new funC("add3")
 	.onErroR()
 	.publish();
 
-
 new claS("add3")
 	.description(`
 		increments input number by three
@@ -83,9 +84,6 @@ new claS("add3")
 	.onErroR()
 	.publish();
 
-
-
-
 new apI("add3")
 	.description(`
 		increments input number by three
@@ -114,9 +112,86 @@ new apI("add3")
 		)
 	.onErroR()
 	// .domain("www.add3.com")
+	.publish()
 	.price({								// requestors must be registered
 		request: 0.001 // 1/10th penny
 		second:  0.001 // 1/10th penny
 	})
-	.publish();
+	.deploy();
 
+/*
+	multiple apis are deployable and become accessible through 
+	devcore deployment services
+*/
+new apI("sub3")
+	.description(`
+		decrements input number by three
+		`)
+	.examplE(`
+		console.log(sub3(3) === 0)
+		`)
+	.speC(
+		`subtracts 3 positives`,
+		[0,3],
+		[3,6],
+		[1,4]
+		)
+	.constrainT(
+		`input is positive`, 		n => n >= 0,
+		`input is less than 256`,	n => n <= 255
+		)
+	.blocK(
+		n => n - 3
+		)
+	.validatE(
+		n => n
+		)
+	.defaulT(
+		n => 1
+		)
+	.onErroR()
+	// .domain("www.sub3.com")
+	.publish()
+	.price({			// requestors must be registered
+		request: 	0.001,// 1/10th penny
+		second:  	0.001,// 1/10th penny
+	})
+	.deploy();
+
+
+
+new system("add3")
+	.description(`
+		increments input number by three
+		`)
+	.examplE(`
+		console.log(add3(3) === 6)
+		`)
+	.speC(
+		`increases 3 positives`,
+		[0,3],
+		[3,6],
+		[1,4]
+		)
+	.constrainT(
+		`input is positive`, 		n => n >= 0,
+		`input is less than 256`,	n => n <= 255
+		)
+	.blocK(
+		n => n + 3
+		)
+	.validatE(
+		n => n
+		)
+	.defaulT(
+		n => 1
+		)
+	.onErroR()
+	// .domain("www.add3.com")
+	.publish()
+	.price({								// requestors must be registered
+		request: 0.001 // 1/10th penny
+		second:  0.001 // 1/10th penny
+	})
+	.deploy()
+	.boot();
