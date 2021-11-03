d1f2d59095eb328b23787054d644f3fc0c79b837
neur0plasticity <you@example.com>
2021-11-02 20:54:52 -0700

    construct funC and claS need testing

--- alg.js
+++ alg.js
@@ -1,44 +1,7 @@
 const prgM = require("./prgM.js");
 module.exports = class alG extends prgM {
-
 	constructor(algname){
 		this.props.algname = algname;
-	}
-	publish(){
-		// confirms with registry the algorithm is unique
-		// checks all props not empty
-		// ensures no errors, and haults if any errors
-
-		// console.log(this.props.speC[0][0])
-		this.props.speC.forEach((element,index)=>{
-			this.props.speC[index].slice(1).forEach((IO_assertion)=>{
-				const input = IO_assertion[0];
-				const output = IO_assertion[1];
-				if (output !== this.props.blocK(input)) this.errors.push(`IO_assertion failed: ${IO_assertion}`);
-			});
-		});
-		if (this.errors.length > 0) {
-			throw new Error(...this.errors)
-		}
-		// publish is the last executable chain so it doesn't return this variable
-
-		// needs to check alg existence in database
-		console.log("published ", this.algname);
-		return this.props.blocK; 
-	}
-	description(descr){
-		(typeof descr === "string") || this.errors.push("prop description not string");
-		(descr.length > 0)			|| this.errors.push("prop description empty");
-		(descr.length < 500)		|| this.errors.push("prop description exceeds 500 char limit");
-		this.props.description.push(descr);
-		return this;
-	}
-	examplE(example){
-		(typeof example === "string") || this.errors.push("prop example not string");
-		(example.length > 0)			|| this.errors.push("prop example empty");
-		(example.length < 500)		|| this.errors.push("prop example exceeds 500 char limit");
-		this.props.examplE.push(example);
-		return this;	
 	}
 	speC(spec /*, assertions*/){
 		(typeof spec === "string")  || this.errors.push("prop spec not string");
